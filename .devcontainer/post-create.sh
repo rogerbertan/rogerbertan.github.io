69741c98012679b90c2405162fd9950579dab57c
rogerbertan <roger.bertan@gmail.com>
2026-08-14 01:11:15 -0300

    chore: drop the node build steps from the deploy workflow

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -f package.json ]; then
-  bash -i -c "nvm install --lts && nvm install-latest-npm"
-  npm i
-  npm run build
-fi
-
 # Install dependencies for shfmt extension
 curl -sS https://webi.sh/shfmt | sh &>/dev/null
 
